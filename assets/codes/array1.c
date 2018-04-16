3b675fc126fdfca58047e60ba80412be404ffbf7
VerasUnB <thiago.machado@cjr.org.br>
2018-04-16 17:31:55 -0300

    Starting new array code

--- assets/codes/array1.c
+++ assets/codes/array1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int n = 31230;
-    int array[3] = {1, 4 ,-8};  
-    printf("%d\n",array[n]); // Irá ocorrer falha de segmentação             }
+    int a[] = {1, 4, -8};
+    int b[3], i;
+
+    for(i = 0; i < 3; i++){
+        b[i] = a[i] * 2;
+    }
+
+    for( i = 0; i < 3; i++){
+    	printf("a[%d] = %d\n",i,a[i]);
+    }
+    printf("---------------\n");
+
+    for( i = 0; i < 3; i++){
+    	printf("b[%d] = %d\n",i,b[i]);
+    }
     return 0;
 }                                           
---
+++ assets/codes/array2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* Programa */
+int main(){
+	int n,i, resp = 0;
+	scanf("%d", &n);
+
+	int turmaA[n], turmaB[n];
+
+	for(i = 0; i < n; i++)
+		scanf("%d", &turmaA[i]);
+	
+	for(i = 0; i < n; i++)
+		scanf("%d", &turmaB[i]);
+	
+	for(i = 0; i < n; i++){
+		if(turmaA[i] > turmaB[i]){
+			resp++;
+		}
+	}
+	
+	printf("A turma A possui %d pontos\n",resp);	
+	printf("A turma B possui %d pontos\n",n-resp);	
+
+    
+    return 0;
+}                                           
