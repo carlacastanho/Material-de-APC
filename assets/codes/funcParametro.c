aa98bde2806447276f717484a119d5c9292e84a4
Gguidini <giovanni.guidini@gmail.com>
2018-04-21 18:07:27 -0300

    Added section 'Vetores como Parametros de funções' na aula 7

---
+++ assets/codes/funcParametro.c
@@ -0,0 +1,19 @@
+/* returns the max between two numbers */
+int max(int a, int b){
+    return a > b ? a : b;
+}
+/* returns the biggest element in array */
+int maxElement(int* array, int n){
+    int i;
+    int max = array[0];
+    for(i = 1; i < n; i++){
+        max = max(array[i], max);
+    }
+    return max;
+}
+
+int main(){
+    int array[] = {0, -1, 3, -10, 4, 22, -5, 2};
+    int maior = maxElement(array, 8);
+    prontf("O maior elemento é %d\n", maior);
+}
