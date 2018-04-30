7ec934dbd59e414f5d681b35f8b860b8d7303dff
VerasUnB <thiago.machado@cjr.org.br>
2018-04-30 00:28:32 -0300

    Fixing some mistakes

--- assets/codes/aula7/buscaLinear.c
+++ assets/codes/aula7/buscaLinear.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main () {
     int i, valor, achou;
-    int conjunto[] = {10,14,19,26,27,31,33,35,42,44};
-    valor = 33;
-
+    int conjunto[] = {3,6,7,10,4,12,9,5,8};
+    valor = 5;
+     
     i = 0;
     achou = 0;
-
+     
     while ((i < 10) && (!achou)) {
         if (conjunto[i] == valor)
             achou = 1;
         else
             i++;
     }
-
+     
     if (achou)
         printf("%d encontrado na posicao %d.\n", valor, i + 1);
     else
