f19b875ac7bfe96df103faaf314b95e88ad119a8
Gguidini <giovanni.guidini@gmail.com>
2018-05-06 22:37:03 -0300

    Copying everything again because Veras didn't have a working Git, but it's ok now.

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -18,3 +18,12 @@ function animation_prev(id, n){
 		}
 	}
 }
+
+function create_animation_block(path, id, n){
+	html = '<div id = "' + id + '1"><img src="' + path + '1.png" class = "img-responsive"></div>';
+	for(var i = 2; i <= n; i++)
+		html += '<div id = "' + id + i.toString() + 'style="display: none;"><img src="' + path + i.toString() + '.png"class = "img-responsive"></div>';	
+	
+	document.getElementById(id).innerHTML= html;
+	//document.getElementById(id).innerHTML = "PENIS";
+}
