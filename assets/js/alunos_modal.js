f2e0abf9d26cabca377d79c774b0d22c0c4d5229
Gguidini <giovanni.guidini@gmail.com>
2019-07-13 20:21:07 -0300

    Add alunos area - test version

---
+++ assets/js/alunos_modal.js
@@ -0,0 +1,67 @@
+// Modal inicial que aparece quando a página carrega
+const modal = document.getElementById("auth-modal");
+modal.classList.toggle('show');
+
+function auth() {
+    // Removes previous login error, if any
+    document.getElementById('login-error-alert').classList.add('hide');
+    document.getElementById('login-invalid-alert').classList.add('hide');
+    // Collects field values
+    let login = document.getElementById("email").value;
+    let pwd = document.getElementById("pwd").value;
+    if(validadeLogin(login)) {
+        // TODO: Response must be an API call
+        let response = {
+            userExists: true,
+            userInfo: {
+                firstname: 'Giovanni',
+                lastname: 'Guidini',
+                matricula: 160122660,
+                photourl: 'userpic.codeforces.com/765049/avatar/a3fcd62d459c9d88.jpg',
+                handle_cf: 'Gguidini',
+                handle_uri: 'Ñ lembro',
+                email: 'email@email.com',
+                grades: {
+                    prova: [8, 8.5, 9],
+                    trab: [7.5, 7.9],
+                    lista: [10, 10, 10, 9.8, 9.5, 6],
+                },
+            },
+            news: [
+                {
+                    id: 103535242,
+                    date: Date(2019, 06, 05),
+                    type: 'Notícia',
+                    content: 'Mostra de jogos no dia 05 de maio, auditório da FT',
+                },
+                {
+                    id: 10353452,
+                    date: Date(2019, 06, 04),
+                    type: 'Aviso',
+                    content: 'Não teremos aula amanha porque não quero ver a cara de vocês',
+                }
+            ]
+        }
+        if (response.userExists == true) {
+            dismissModal();
+            loadDinamicContent(response);
+        } else {
+            document.getElementById('login-error-alert').classList.remove('hide');
+            document.getElementById("pwd").value = '';
+        }
+    } else {
+        document.getElementById('login-invalid-alert').classList.remove('hide');
+    }
+    return ;
+}
+
+function dismissModal() {
+    modal.classList.remove('show');
+    modal.classList.add('fade');
+}
+
+function validadeLogin(login) {
+    return String(login).split('').reduce((isDigit, char) => {
+        return '0123456789'.includes(char) & isDigit;
+    }, true);
+}
---
+++ assets/js/alunos_react.js
@@ -0,0 +1,167 @@
+'use strict';
+
+class Profile extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            firstname: props.firstname,
+            lastname: props.lastname,
+            matricula: props.matricula,
+            photourl: props.photourl,
+            handle_cf: props.handle_cf,
+            handle_uri: props.handle_uri,
+            email: props.email,
+        }
+    }
+
+    render() {
+        return (
+            <div className="panel panel-default panel-blue">
+                <h4 className="panel-header">Perfil</h4>
+            <div className="container">
+                <div className="media-left">
+                    {   this.state.photourl != '' ?
+                        <img src={"http://" + this.state.photourl} alt="Avatar" className="media-object"/> :
+                        <span className="glyphicon glyphicon-user media-object"></span>
+
+                    } 
+                </div>
+                <div className="media-body">
+                    <p className="media-heading name-text">
+                    {this.state.firstname} {this.state.lastname}
+                    </p>
+                    { this.state.handle_cf !== '' ?
+                        <p className="handle-text">
+                            <img className="tiny-icon" src="assets/images/codeforces_icon.png" alt="@"></img>
+                            {this.state.handle_cf}
+                        </p> :
+                        ''
+                    }
+                    {
+                        this.state.handle_uri !== '' ?
+                        <p className="handle-text">
+                            <img className="tiny-icon" src="assets/images/uri_icon.png" alt="@"></img>
+                            {this.state.handle_uri}
+                        </p> :
+                        ''
+                    }
+                </div>
+                <div>
+                    <p>
+                    <button className="btn-edit btn">
+                        <span className="glyphicon glyphicon-edit"></span>
+                    </button> Email: <span className="handle-text">{this.state.email}</span>
+                    </p>
+                </div>
+            </div>
+            </div>);
+    }
+}
+
+class Grades extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            grades: props,
+        }
+    }
+
+    render() {
+        const prova_items = this.state.grades.prova.map((g, idx) => {
+            return (
+                <li key={idx} className="list-group-item">
+                <span className="list-item">Prova {idx+1} </span>{g}
+                </li>
+            )
+        });
+        const trab_items = this.state.grades.trab.map((g, idx) => {
+            return (
+                <li key={idx} className="list-group-item">
+            <span className="list-item">Trabalho (parte {idx+1}) </span>{g}
+                </li>
+            )
+        });
+        const list_items = this.state.grades.lista.map((g, idx) => {
+            return (
+                <li key={idx} className="list-group-item">
+            <span className="list-item">Lista {idx+1}</span>{g}
+                </li>
+            )
+        });
+        return (
+            <div className="panel panel-default panel-blue">
+                <h4>Notas</h4>
+                <ul className="list-group">
+                    {prova_items}
+                    {list_items}
+                    {trab_items}
+                </ul>
+            </div>
+        )
+    }
+}
+
+class News extends React.Component {
+    render() {
+        const newsArray = Object.values(this.props);
+        const news_item = newsArray.map((g) => {
+            return (
+                <li key={g.id} className={"list-group-item"}>
+                <div>
+                    <span className="list-item">{g.type + ':'}</span> 
+                    <p>{g.content}</p>
+                    <p className="date-item">{g.date}</p>
+                </div>
+                </li>
+            )
+        });
+
+        return (
+            <div className="panel panel-default panel-blue">
+                <h4>Notícias e Avisos</h4>
+                <ul className="list-group">
+                    {news_item}
+                </ul>
+            </div>
+        )
+    }
+}
+
+class Activities extends React.Component {
+    state = {};
+
+    componenDidMount() {
+        // TODO: Make API call to Codeforces
+    }
+
+    render() {
+        return (
+        <div className="panel panel-default panel-blue">
+            <h4>Atividades</h4>
+            <ul className="list-group">
+                <li className="list-group-item">Não sei pegar as listas ainda</li>
+            </ul>
+        </div>
+        );
+    }
+        
+}
+
+function loadDinamicContent(data){
+    ReactDOM.render(
+        <Profile {...data.userInfo}/>,
+        document.getElementById("student-root")
+    );
+    ReactDOM.render(
+        <Grades {...data.userInfo.grades}/>,
+        document.getElementById("grades-root")
+    );
+    ReactDOM.render(
+        <News {...data.news}/>,
+        document.getElementById("news-root")
+    );
+    ReactDOM.render(
+        <Activities />,
+        document.getElementById("activities-root")
+    );
+}
